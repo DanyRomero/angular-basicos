7b1368e2a72bed851c2c9a83a5640060b320a2e2
Daniela Romero <danyrp45@gmail.com>
2022-11-22 12:31:01 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'app-root',//indica ubicación del componente
+  templateUrl: './app.component.html', // template indica la fuente del HTML
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+  
 }
