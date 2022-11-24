cb43e8ab6d5b3185167709bf7321fc9a9c0adf0a
Daniela Romero <danyrp45@gmail.com>
2022-11-24 14:11:38 -0600

    create new module and separate the interface

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
 
-interface Personaje {
-  nombre: string;
-  poder: number;
-}
+
 
 @Component({
   selector: 'app-main-page',
@@ -21,20 +19,8 @@ export class MainPageComponent {
     }
   ]
   nuevo: Personaje = {
-    nombre: "",
-    poder: 0,
+    nombre: "Maestro Roshi",
+    poder: 10000,
   }
-  agregar(){
-    if(this.nuevo.nombre.trim().length === 0){
-      return;
-    }
-    console.log(this.nuevo)
-    this.personajes.push(this.nuevo)
-    this.nuevo = {
-      nombre: "",
-      poder: 0,
-    }
-  }
-
  
 }
